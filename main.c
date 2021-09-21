5722a5e11d06ddf303beda5e5277161421fa8774
Erik Agsjö <erik.agsjo@gmail.com>
2021-09-21 22:31:02 +0200

    Image loading

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include "tigr.h"
 
 void tigrMain() {
@@ -6,8 +7,18 @@ void tigrMain() {
     int textHeight = tigrTextHeight(tfont, message);
     int textWidth = tigrTextWidth(tfont, message);
 
+    Tigr* logo = tigrLoadImage("timogr.png");
+    if (!logo) {
+        tigrError(0, "Failed to load image: %d", errno);
+    }
+
     while (!tigrClosed(screen)) {
+        int numTouches = 0;
+        
         tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+
+        int logoX = (screen->w - logo->w) / 2;
+        tigrBlitAlpha(screen, logo, logoX, 10, 0, 0, logo->w, logo->h, numTouches > 0 ? 0.5 : 1);
 
         int textX = (screen->w - textWidth) / 2;
         int textY = (screen->h - textHeight) / 2;
